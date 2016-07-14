a2a2813a228d9a9afcea04991364c0baaef373d4
Cássio Santos <sss.cassio@gmail.com>
2016-07-14 00:47:45 -0300

    1-bit Full Adder (done)

--- Labs/Lab00/lab0/verilog/FA.v
+++ Labs/Lab00/lab0/verilog/FA.v
@@ -3,7 +3,7 @@
 // Lab 0, 2016.1
 // Module: FA.v
 // Desc: 1-bit Full Adder
-//       You may only use structural verilog in this module.       
+//       You may only use structural verilog in this module.
 //-----------------------------------------------------------------------------
 module FA(
     input A, B, Cin,
@@ -11,6 +11,19 @@ module FA(
 );
    // Structural verilog only!
    /********YOUR CODE HERE********/
+    // Desc: Sum = (A xor B) xor Cin
+    //       Cout = [(A xor B) and Cin] or (A and B)
+
+    wire AxorB, AandB, AxorBandCin;
+
+    //Share Wire
+    xor (AxorB, A, B);
+    //Sum struct
+    xor (Sum, AxorB, Cin)
+    //Cout struct
+    and (AandB, A, B);
+    and (AxorBandCin, AxorB, Cin);
+    or (Cout, AxorBandCin, AandB);
+
    /********END CODE********/
 endmodule
-
