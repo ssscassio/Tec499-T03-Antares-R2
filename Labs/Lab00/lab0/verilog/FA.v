9746abfb0ede939bcf91d57f19c0570614bb47c7
Cássio Santos <sss.cassio@gmail.com>
2016-07-14 00:20:18 -0300

    Lab 0 first commit

---
+++ Labs/Lab00/lab0/verilog/FA.v
@@ -0,0 +1,16 @@
+//-----------------------------------------------------------------------------
+// UEFS TEC 499
+// Lab 0, 2016.1
+// Module: FA.v
+// Desc: 1-bit Full Adder
+//       You may only use structural verilog in this module.       
+//-----------------------------------------------------------------------------
+module FA(
+    input A, B, Cin,
+    output Sum, Cout
+);
+   // Structural verilog only!
+   /********YOUR CODE HERE********/
+   /********END CODE********/
+endmodule
+
