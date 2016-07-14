4c09770c06125afea87af1c17b7c115c97c617eb
Cássio Santos <sss.cassio@gmail.com>
2016-07-14 13:57:15 -0300

    ml505top, added instance of Full Adder (done)

--- Labs/Lab00/lab0/verilog/ml505top.v
+++ Labs/Lab00/lab0/verilog/ml505top.v
@@ -4,7 +4,7 @@
 module ml505top (
    input  [3:0] Switch,
    input  [11:0] KEY,
-   input  clock_50MHz,   
+   input  clock_50MHz,
    output [7:0] LEDM_R,
    output [4:0] LEDM_C,
    output LED_R,
@@ -29,9 +29,9 @@ and (LEDM_C_inv[0], Switch[0], Switch[1]);
 
 
 /* This shows an example of module instantiation (you will need to
-*  implement Mux2_1 for this to function). If you have correctly 
-*  implemented Mux2_1, you should be able to use switch 'SW2' to 
-*  select whether the value of switch 'SW0' or 'SW1' is displayed 
+*  implement Mux2_1 for this to function). If you have correctly
+*  implemented Mux2_1, you should be able to use switch 'SW2' to
+*  select whether the value of switch 'SW0' or 'SW1' is displayed
 *  on LED (R0,C1). */
 Mux2_1 gpio_mux(
    .A(Switch[0]),
@@ -43,15 +43,20 @@ Mux2_1 gpio_mux(
 
 
 /* Next, implement a full adder in FA.v. Then, instantiate the FA module
-*  below such that the inputs to the adder are GPIO DIP switches 0-2 
-*  (recall the number is off by one), the sum is displayed on LED (R0,C2), 
-*  and the carry is displayed on LED (R0,C3). Then delete the specified 
+*  below such that the inputs to the adder are GPIO DIP switches 0-2
+*  (recall the number is off by one), the sum is displayed on LED (R0,C2),
+*  and the carry is displayed on LED (R0,C3). Then delete the specified
 *  line once your done */
 
 /**********YOUR CODE HERE************/
+FA two_bits_add(
+    .A(Switch[0]),
+    .B(Switch[1]),
+    .Cin(Switch[2]),
+    .Sum(LEDM_C_inv[2]),
+    .Cout(LEDM_C_inv[3])
+);
 /**********END CODE**************/
-
-assign LEDM_C_inv [3:2] = 2'b0;  // delete this once you instantiate your full adder
 
 /* Now for the adder instantiation and testing
 *  The Adder module is already instantiated for you.
@@ -68,7 +73,7 @@ reg [CWidth-1:0]    Count;
 wire [Width-1:0]    CUTResult;
 wire [Width-1:0]    ExpectedResult;
 wire                CUTCout;
-wire                ExpectedCout;	
+wire                ExpectedCout;
 
 assign Clock = clock_50MHz;
 assign Reset = KEY[3] | KEY[2] | KEY[1] | KEY[0];
@@ -97,6 +102,6 @@ end
 assign LED_G = CUTResult == ExpectedResult && CUTCout == ExpectedCout;
 
 //GPIO RGB LED will light up red if you Adder is wrong
-assign LED_R = {~LED_G};  
+assign LED_R = {~LED_G};
 assign LED_B = 1'b0;
 endmodule
