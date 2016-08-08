80db1a503170188f98664a6b574487039690791e
Cássio Silva de Sá Santos <sss.cassio@gmail.com>
2016-08-07 21:12:51 -0300

    Some changes on Pow Test, Working on MARS 4.5

--- Tests/pow/pow_c.asm
+++ Tests/pow/pow_c.asm
@@ -1,21 +1,29 @@
-; Cássio Santos, Potencial
-;
-;function pow(int x, int y){
-;  int result = 1;
-;  for(int i = 0; i!=y ; i++){
-;  result = result*x;
-;  }
-;  return result;
-;}
-
+# Cássio Santos, Potencial
+#
+#function pow(int x, int y){
+#  int result = 1;
+#  for(int i = 0; i!=y ; i++){
+#  result = result*x;
+#  }
+#  return result;
+#}
+main: 
+      LI $a0, 2
+      LI $a1, 3
+      jal pow
+      j exit
+      
 pow:
-      ADD $s0, $zero, $a0 ; s0 = a0 ; variable X
-      ADD $s1, $zero, $a1 ; s1 = a1 ; variable Y
-      LI $s2, 1           ; s2 = 1 ; variable result
-      LI $s3, 0           ; s3 = 0 ; variable i
-L1:   BNE $s3, $s1, L2    ; if(s3 != s1) go to L2
-      MUL $s2, $s2, s0    ; result = result*x
-      ADDI $s3, $s3 , 1   ; i = i + 1
-      J L1                ; Next iteration of loop
-L2:   MOVE $v0, $s2       ; v0 = s2
-      JR $ra              ; return
+      ADD $s0, $zero, $a0 
+      ADD $s1, $zero, $a1 
+      LI $s2, 1           
+      LI $s3, 0           
+L1:   
+      BEQ $s3, $s1, L2    
+      MUL $s2, $s2, $s0    
+      ADDI $s3, $s3 , 1   
+      J L1                
+L2:   
+      MOVE $v0, $s2       
+      JR $ra              
+exit:
