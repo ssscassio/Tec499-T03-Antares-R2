b3075b7c237021f643eb8fa1bcda120a3b80aa1b
Cássio Silva de Sá Santos <sss.cassio@gmail.com>
2016-08-07 22:49:59 -0300

    Fibonacci Asm Done, Tested on MARS 4.5

--- Tests/Fibonacci/fibonacci_c.asm
+++ Tests/Fibonacci/fibonacci_c.asm
@@ -1,8 +1,39 @@
-; Cássio Santos, Fibonacci Recursivo
-;
-;function fibonacci(int n){
-;  if(n == 1 || n==2)
-;     return 1;
-;  else
-;     return fibonacci(n-1) * fibonacci(n-2);
-;}
+# Cássio Santos, Fibonacci Recursivo
+#
+#function fibonacci(int n){
+#  if(m<2)
+#     return 1;
+#  else
+#     return fibonacci(n-1) * fibonacci(n-2);
+#}
+
+main:
+  ADD $a0, $zero, 7
+  JAL fib
+  J exit
+  
+fib :
+  ADDI $sp , $sp , -12
+  sw $s0, 0($sp) #n
+  sw $s1, 4($sp)
+  sw $ra, 8($sp)
+  slti $t0, $a0, 2
+  beq $t0, $zero, else
+  add $v0, $zero, $a0
+  j exit
+  
+else:
+  addi $s0, $a0, 0
+  addi $a0, $a0, -1
+  jal fib
+  addi $s1, $v0, 0
+  addi $a0, $s0, -2
+  jal fib
+  add $v0, $s1, $v0
+
+exit:
+  lw $s0, 0($sp)
+  lw $s1, 4($sp)
+  lw $ra, 8($sp)
+  addi $sp, $sp, 12
+  jr $ra
