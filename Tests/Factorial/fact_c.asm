53dbd016eefaa812fd264e95fc324dd0011b75b6
Cássio Santos <sss.cassio@gmail.com>
2016-08-08 13:35:03 -0300

    Factorial asm Code Based on Khaick's code, Working on MARS 4.5

--- Tests/Factorial/fact_c.asm
+++ Tests/Factorial/fact_c.asm
@@ -1,27 +1,36 @@
-#Wanderson Silva, Raiz
-#int square_root(int rad){
-#  int index=0;
-#  int result = 0;
-#  while(rad != 0){
-#    int n = index*2+1;
-#    rad = rad-n;
-#    result++;
-#    index++;
-#  }
-#  return result;
+# Cássio Santos, Fibonacci Recursivo
+#
+#function factorial(int n){
+#  if(n<=1)
+#     return 1;
+#  else
+#     return n * fatorial(n-1);
 #}
-square:
-    ADD $s0, $zero, $a0  # s0 = a0 # variable rad
-    LI $s1, 0           # s1 = 0 # variable index
-    LI $s2, 0           # s2 = 0 # variable result
-    LI $s3, 0           # s3 = 0 # variable n
-L1: BNE $s0, $zero, L2
-    LI $t0, 2		# t0 = 2
-    MUL $s3, $s1, $t0	# s3 = s1 * t0 --> n = index*2
-    ADDI $s3,$s3, 1	# s3 = s3 + 1  --> n = n + 1
-    SUB  $s0, $s0, $s3	# s0 = s0 - s3 --> rad = rad - n
-    ADDI $s2, ,$s2, 1	# result++
-    ADDI $s1, $s1, 1	# index++
-    J L1
-L2: MOVE $v0, $s2       # v0 = s2
-    JR $ra              # return
+main:
+   li $a0,5 
+   jal FACT    
+   j END	
+FACT:
+    sub $sp,$sp,8   # Ajust the Stack Pointer
+    sw $ra, 4($sp)   # Store the linked address
+    sw $a0, 0($sp)   # Store the argument of Fact(N)
+
+   #Comparison
+   slti $t0,$a0,1   # if n <= 1: $t0 = 1
+   beq $t0,$zero,REC   # if n >= 1, branch to REC -> (recursive)
+   li $v0,1      # if n > 1
+
+   add $sp,$sp,8   # Release the Stack
+
+   jr $ra      # Return
+REC:
+   subi $a0,$a0,1   # New argument for Fact. N-1.
+   jal FACT      # Recursive call
+
+   lw $a0, 0($sp)   # Get stored argument
+   lw $ra, 4($sp)   # Get stored linked address
+   add $sp,$sp,8   # Frees up space in stack
+
+   mul $v0,$a0,$v0   # Multiplies actual argument * fact(N-1)
+   jr $ra            # Jumps to linked address
+END:
