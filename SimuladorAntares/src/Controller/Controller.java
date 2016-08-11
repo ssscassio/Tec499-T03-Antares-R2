b4998bf280930f164fd09a1b12c9bf3bb488cc7d
khaickbrito <khaickbrito@gmail.com>
2016-08-10 22:28:16 -0300

    Starting Simulator project

--- SimuladorAntares/src/Controller/Controller.java
+++
@@ -1,7 +0,0 @@
-package Controller;
-
-/**
- * Created by Wanderson on 10/08/16.
- */
-public class Controller {
-}
--- SimuladorAntares/src/Model/Instructions.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-/**
- * Created by Wanderson on 10/08/16.
- */
-public class Instructions {
-}
