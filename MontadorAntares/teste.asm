2d8f5da2a707ec02a88a0fbb3074ec48a7bb7455
wandsilva <wanderson.uefs@gmail.com>
2016-08-12 19:52:10 -0300

    Created the instruction LUI and finished the pseudo instructions convert. It's missing the pseudo instruction LA.

--- MontadorAntares/teste.asm
+++ MontadorAntares/teste.asm
@@ -1,14 +1,4 @@
-; Cássio Santos, Potencial
-;
-;function pow(int x, int y){
-;  int result = 1;
-;  for(int i = 0; i!=y ; i++){
-;  result = result*x;
-;  }
-;  return result;
-;}
-
-pow:
-      rotr $s0, $S1, 32
-      sll $s0, $s1, 4
-
+labelX:
+main: 
+     la $s3, labelX
+exit: 
