e350dac2b3411a8addf57551fafc13b901495085
Khaick Brito <khaickbrito@gmail.com>
2016-09-18 13:39:20 -0300

    Updating control_unit

--- ProcessorAntares/control_unit.v
+++ ProcessorAntares/control_unit.v
@@ -10,73 +10,113 @@
 // Outputs:
 // 	out: A microprogram to command datapath's structures
 //-----------------------------------------------------------------------------
+include "Opcode.vh"
 `ifndef _control_unit
 `define _control_unit
 
 module control_unit(
   input [5:0] opCode,
-  output reg [1:0] aluOp,
+  output reg [1:0] aluOp, //Alu Control:  00:lw,sw   01:beq    10:arithmetic
   output reg regDst,memRead,memToReg,memWrite,aluSrc,regWrite,
   output reg beq,bne,
   output reg jump
   );
 
   always @ ( * ) begin
-  //Default settings
-    regDst <= 1'b1;
-    beq <= 1'b0;
-    bne <= 1'b0;
-    memRead <= 1'b0;
-    memToReg <= 1'b0;
-    aluOp[1:0] <= 2'b10;
-    memWrite <= 1'b0;
-    aluSrc <= 1'b0;
-    regWrite <= 1'b1;
-    jump <= 1'b0;
-
     case (opCode)
-    //JUMP
-      6'b000010: begin
+    //R-TYPE
+    	`RTYPE: begin
+        regDst <= 1'b1;
+        beq <= 1'b0;
+        bne <= 1'b0;
+        memRead <= 1'b0;
+        memToReg <= 1'b0;
+        aluOp[1:0] <= 2'b10;
+        memWrite <= 1'b0;
+        aluSrc <= 1'b0;
+        regWrite <= 1'b1;
+        jump <= 1'b0;
+    	end
+
+    //LI  (addiu $4, $zero, C)
+    	`LI: begin
+
+    	end
+
+    //J
+    	`J: begin
         jump <= 1'b1;
-      end
-    //ADDI
-      6'b001000: begin
-        regDst   <= 1'b0;
-        aluOp[1] <= 1'b0;
-        aluSrc   <= 1'b1;
-      end
-    //BEQ
-      6'b000100: begin
-        aluOp[0]  <= 1'b1;
-        aluOp[1]  <= 1'b0;
-        beq <= 1'b1;
-        regWrite  <= 1'b0;
-      end
-    //BNE
-      6'b000101: begin
-        aluOp[0]  <= 1'b1;
-        aluOp[1]  <= 1'b0;
-        bne <= 1'b1;
-        regWrite  <= 1'b0;
-      end
-    //LW
-      6'b100011: begin
-        memRead  <= 1'b1;
-        regDst   <= 1'b0;
-        memToReg <= 1'b1;
-        aluOp[1] <= 1'b0;
-        aluSrc   <= 1'b1;
-      end
+    	end
+    //JAL
+    	`JAL: begin
+
+    	end
+
     //SW
-      6'b101011: begin
+    	`SW: begin
+        aluOp[1:0] <= 2'b00;
         memWrite <= 1'b1;
         aluOp[1] <= 1'b0;
         aluSrc   <= 1'b1;
         regWrite <= 1'b0;
+    	end
+
+    //LW
+    	`LW: begin
+      memRead  <= 1'b1;
+      regDst   <= 1'b0;
+      memToReg <= 1'b1;
+      aluOp[1] <= 1'b0;
+      aluSrc   <= 1'b1;
+    	end
+
+    //SLTI
+    	`SLTI: begin
+
+    	end
+
+    //BEQ
+    	´BEQ: begin
+        aluOp[0]  <= 1'b1;
+        aluOp[1]  <= 1'b0;
+        beq <= 1'b1;
+        regWrite  <= 1'b0;
+    	end
+
+    //BNE
+    	´BNE: begin
+        aluOp[0]  <= 1'b1;
+        aluOp[1]  <= 1'b0;
+        bne <= 1'b1;
+        regWrite  <= 1'b0;
+    	end
+
+    //ADDI
+    	`ADDI: begin
+        regDst   <= 1'b0;
+        aluOp[1] <= 1'b0;
+        aluSrc   <= 1'b1;
+    	end
+
+    //SUBI
+      `SUBI: begin
+
       end
-    //JR
-      6'
-    
+
+    //JR    (Tipo R)
+    	`JR: begin
+
+    	end
+
+    //MFHI
+    	`MFHI: begin
+
+    	end
+
+    //MOVE  (add $1, $2, $zero)
+    	`MOVE: begin
+
+    	end
    endcase
  end
 endmodule
